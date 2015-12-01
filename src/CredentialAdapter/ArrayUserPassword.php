1b339e5904d6007f09e61ae83519efa2083724f6
Witold Wasiczko <witold.wasiczko@rst.com.pl>
2015-12-01 16:02:39 +0100

    Update phpdoc

--- src/CredentialAdapter/ArrayUserPassword.php
+++ src/CredentialAdapter/ArrayUserPassword.php
@@ -4,16 +4,27 @@ namespace PhpMiddleware\HttpAuthentication\CredentialAdapter;
 
 final class ArrayUserPassword implements UserPasswordInterface
 {
+    /**
+     * @var array
+     */
     protected $users;
 
+    /**
+     * @param array $users
+     */
     public function __construct(array $users)
     {
         $this->users = $users;
     }
 
+    /**
+     * @param mixed $username
+     * @param mixed $password
+     *
+     * @return bool
+     */
     public function authenticate($username, $password)
     {
         return isset($this->users[$username]) && $this->users[$username] === $password;
     }
-
 }
--- src/CredentialAdapter/UserPasswordInterface.php
+++ src/CredentialAdapter/UserPasswordInterface.php
@@ -4,5 +4,11 @@ namespace PhpMiddleware\HttpAuthentication\CredentialAdapter;
 
 interface UserPasswordInterface
 {
+    /**
+     * @param mixed $username
+     * @param mixed $password
+     *
+     * @return bool
+     */
     public function authenticate($username, $password);
 }
